dd0761af4361346923f23355df6fdc0d41414423
MAXVvidik <maksimvvedenskij@gmail.com>
2022-11-16 01:58:18 +0400

    первая проверка фз 13(v0.1)

--- src/main/java/ru/practicum/shareit/item/validate/ValidateItemData.java
+++ src/main/java/ru/practicum/shareit/item/validate/ValidateItemData.java
@@ -18,7 +18,7 @@ public class ValidateItemData {
         setItem(item);
         if (isCorrectName() && isCorrectDescription() && isCorrectAvailable()) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
@@ -26,7 +26,7 @@ public class ValidateItemData {
     public boolean isCorrectName() {
         if (item.getName() != null && !item.getName().isEmpty()) {
             return true;
-        }else{
+        } else {
             log.warn("Ошибка во входных данных. Некорректное название вещи.");
             return false;
         }
@@ -35,7 +35,7 @@ public class ValidateItemData {
     public boolean isCorrectDescription() {
         if (item.getDescription() != null && !item.getDescription().isEmpty()) {
             return true;
-        }else{
+        } else {
             log.warn("Ошибка во входных данных. Описание вещи отсутствует или не заполнено");
             return false;
         }
@@ -44,7 +44,7 @@ public class ValidateItemData {
     public boolean isCorrectAvailable() {
         if (item.getAvailable() == null) {
             return false;
-        }else{
+        } else {
             return true;
         }
     }
